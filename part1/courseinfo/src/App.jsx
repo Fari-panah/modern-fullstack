f8069a01a0a3725cca2a71b8e8dcf3483b7be08d
Fari-panah <panahfari81@gmail.com>
2025-05-20 14:17:03 +0300

    1.1: Course Information, step 1, add three components: Header, Content, and Total

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,5 +1,35 @@
+const Header = (props) => {
+  return(
+    <div>
+      {props.course}
+    </div>
+  )
+
+}
+const Content = (props) => {
+  return(
+    <div>
+      {props.part1}  {props.exercises1}
+      {props.part2}  {props.exercises2}
+      {props.part3}  {props.exercises3}
+    </div>
+  )
+
+}
+const Total = (props) => {
+  return(
+    <div>
+      sum of exercises {props.sum}
+    </div>
+  )
+
+}
+
+
+
 const App = () => {
    const course = 'Half Stack application development'
+  
   const part1 = 'Fundamentals of React'
   const exercises1 = 10
   const part2 = 'Using props to pass data'
@@ -8,16 +38,12 @@ const App = () => {
   const exercises3 = 14
   return(
     <div>
-      <h1>{course}</h1>
-      <p>{part1} {exercises1}</p>
-       <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
-
+      <Header course={course}/>
+      <Content part1={part1} exercises1={exercises1}/>
+      <Content part2={part2} exercises2={exercises2}/>
+      <Content part3={part3} exercises3={exercises3}/>
+      <Total sum = {exercises1 + exercises2 + exercises3}/>
+      
     </div>
   )
 }
