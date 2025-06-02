9478b4586f784135a1a2c2a839836b4c45944c16
Fari-panah <panahfari81@gmail.com>
2025-06-02 10:32:54 +0300

    2.2: Course information step 7, add total

--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -1,7 +1,7 @@
 const Header=({name}) =><h1>{name}</h1>
 const Content =({parts})=>parts.map(part => <Part key={part.id} part={part}/>)
 const Part=({part}) => <p>{part.name} {part.exercises}</p>
-
+const Total= ({parts}) => <p>total of {parts.reduce((s,p) => s +p.exercises,0)} exercises</p>
 
 const Course= ({course}) => {
    
@@ -9,7 +9,7 @@ const Course= ({course}) => {
         <div>
           <Header name={course.name}/>
           <Content parts={course.parts}/>
-    
+           <Total  parts={course.parts}/>
         </div>
     )
 
