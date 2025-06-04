4431e109caf26f29badbc52dd967b312fc8c8d99
Fari-panah <panahfari81@gmail.com>
2025-06-04 14:35:02 +0300

    2.11: The Phonebook Step 6 , useEffect and axios

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,17 +1,26 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Filter from "./components/Filter"
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
+import axios from 'axios'
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+    // { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    // { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    // { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    // { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filterQuery, setFilterQuery] = useState('')
+
+   useEffect(() =>{
+    axios
+    .get('http://localhost:3001/persons')
+    .then(response => {
+      setPersons(response.data)
+    })
+  }, [])
   const handlePersons= (event) =>{
     event.preventDefault()
     if (persons.find(person => person.name === newName)){
@@ -28,6 +37,8 @@ const App = () => {
    const handleNewName = (event)=> setNewName(event.target.value)
    const handleNewNumber = (event) => setNewNumber(event.target.value)
    const handleFilterQuery = (event) => setFilterQuery(event.target.value)
+
+
 
   return (
     <div>
