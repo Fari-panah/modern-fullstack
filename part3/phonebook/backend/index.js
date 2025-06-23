fa3d4f1b17e0955ee1860a63aced7ac521f6a8aa
Fari-panah <panahfari81@gmail.com>
2025-06-23 11:36:07 +0300

    3.3: Phonebook backend step 3

--- part3/phonebook/backend/index.js
+++ part3/phonebook/backend/index.js
@@ -40,6 +40,17 @@ app.get('/info', (request, response) => {
     response.send(`Phonebook has info for ${numberOfPersons} people<br>${new Date()}`)
 
 })
+app.get('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  const person = persons.find(p=> p.id === id)
+  
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+
+})
 
 const PORT = 3001
 app.listen(PORT)
