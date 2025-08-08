717d51206dc147353e2ef8c084d0c07c606e151c
Fari-panah <panahfari81@gmail.com>
2025-08-08 09:48:34 +0300

    3.13: Phonebook database, step 1,3.14: Phonebook database, step 2

--- part3/phonebook/backend/index.js
+++ part3/phonebook/backend/index.js
@@ -1,5 +1,7 @@
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
+const Person = require('./models/person')
 const morgan = require('morgan')
 const app = express()
 
@@ -15,36 +17,39 @@ app.use(express.static('dist'))
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :type :post-data'))
 
-let persons = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
+let persons = []
+    // { 
+    //   "id": "1",
+    //   "name": "Arto Hellas", 
+    //   "number": "040-123456"
+    // },
+    // { 
+    //   "id": "2",
+    //   "name": "Ada Lovelace", 
+    //   "number": "39-44-5323523"
+    // },
+    // { 
+    //   "id": "3",
+    //   "name": "Dan Abramov", 
+    //   "number": "12-43-234345"
+    // },
+    // { 
+    //   "id": "4",
+    //   "name": "Mary Poppendieck", 
+    //   "number": "39-23-6423122"
+    // }
+
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!!</h1>')
 })
 
 app.get('/api/persons', (request, response) => {
-  response.json(persons)
+  Person.find({}).then(persons => {
+    response.json(persons)
+  })
 })
+
 
 app.get('/info', (request, response) => {
     const numberOfPersons = persons.length
@@ -52,16 +57,12 @@ app.get('/info', (request, response) => {
 
 })
 app.get('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  const person = persons.find(p=> p.id === id)
-  
-  if (person) {
+   Person.findById(request.params.id).then(person => {
     response.json(person)
-  } else {
-    response.status(404).end()
-  }
-
+   })
+  
 })
+
 app.delete('/api/persons/:id', (request, response) => {
     const id = request.params.id
     persons = persons.filter(p => p.id !== id)
@@ -70,22 +71,21 @@ app.delete('/api/persons/:id', (request, response) => {
 })
 
 app.post('/api/persons', (request, response) => {
-    const person = request.body
-    console.log("Received new person", person)
+  const body = request.body
+  if (!body.name || !body.number){
+    return response.status(400).json({error: 'name or number missing'})
+  }
+  const person = new Person({
+    name: body.name,
+    number: body.number
+  }) 
+    person.save().then(savedPerson => {
+      response.json(savedPerson)
 
-    if (!person.name || !person.number) {
-        return response.status(400).json({ error: 'Name or number missing' })
-    }
-    const existPerson = persons.find(person => person.name === person.name)
-    if(existPerson){
-        return response.status(400).json({ error: 'name must be unique' })
-    }
-    person.id = Math.floor(Math.random() * 1000000)
-    persons = persons.concat(person)
-
-    response.json(persons)
+    })
+    
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT 
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
--- part3/phonebook/backend/mongo.js
+++ part3/phonebook/backend/mongo.js
@@ -7,34 +7,69 @@ if (process.argv.length < 3) {
 
 const password = process.argv[2]
 
-const url = `mongodb+srv://fullstack:${password}@cluster0.aemsfld.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+const url = `mongodb+srv://fullstack:${password}@cluster0.aemsfld.mongodb.net/peopleApp?retryWrites=true&w=majority&appName=Cluster0`
 
 mongoose.set('strictQuery',false)
 
 mongoose.connect(url)
+.then(() => {
+    console.log("✅ Connected to MongoDB!")
+    //mongoose.connection.close()
+  })
+  .catch(err => {
+    console.error("❌ Connection error:", err.message)
+  })
 
 const personSchema = new mongoose.Schema({
   name: String,
   number: String,
 })
 
-const Person = mongoose.model('Person', personSchema)
-if (process.argv.length === 5) {
-const name = process.argv[3];
-const number = process.argv[4];
-const person = new Person({name,number})
-person.save().then(() => {
-    console.log(`added ${name} number ${number} to phonebook`);
-    mongoose.connection.close()
-  })
-}
+personSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
 
-if (process.argv.length === 3) {
-  Person.find({}).then(result => {
-    console.log('phonebook:')
-    result.forEach(p => {
-      console.log(`${p.name} ${p.number}`)
-    })
-    mongoose.connection.close();
+const Person = mongoose.model('Person', personSchema)
+
+const person = new Person({
+
+     "name": "Arto Hellas", 
+     "number": "040-123456",
+   
+})
+
+person.save().then(result => {
+  console.log('person saved!!!')
+  mongoose.connection.close()
+})
+
+Person.find({}).then(result => {
+  result.forEach(p => {
+    console.log(p);
   })
-}
+  mongoose.connection.close()
+})
+
+// if (process.argv.length === 5) {
+// const name = process.argv[3];
+// const number = process.argv[4];
+// const person = new Person({name,number})
+// person.save().then(() => {
+//     console.log(`added ${name} number ${number} to phonebook`);
+//     mongoose.connection.close()
+//   })
+// }
+
+// if (process.argv.length === 3) {
+//   Person.find({}).then(result => {
+//     console.log('phonebook:')
+//     result.forEach(p => {
+//       console.log(`${p.name} ${p.number}`)
+//     })
+//     mongoose.connection.close();
+//   })
+// }
