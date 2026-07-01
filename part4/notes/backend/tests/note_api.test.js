97cc68d6f01a2ea4095df402ab9e6a1a940163ee
Fari-panah <panahfari81@gmail.com>
2026-07-01 10:46:24 +0300

    add beforeEach test to note_api.test.js

--- part4/notes/backend/tests/note_api.test.js
+++ part4/notes/backend/tests/note_api.test.js
@@ -1,23 +1,42 @@
 
-const { test, after } = require('node:test')
+const { test, after, beforeEach } = require('node:test')
 const assert = require('node:assert')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
+const Note = require('../models/note')
 
 const api = supertest(app)
+const initialNotes = [
+  {
+    content: 'HTML is easy',
+    important: false,
+  },
+  {
+    content: 'Browser can execute only JavaScript',
+    important: true,
+  },
+]
+beforeEach(async () => {
+  await Note.deleteMany({})
+  let noteObject = new Note(initialNotes[0])
+  await noteObject.save()
+  noteObject = new Note(initialNotes[1])
+  await noteObject.save()
+})
 
 test('notes are returned as json', async () => {
   await api
     .get('/api/notes')
     .expect(200)
+    //regex instead of an exact string
     .expect('Content-Type', /application\/json/)
 })
 
 test('all notes are returned', async () => {
   const response = await api.get('/api/notes')
 
-  assert.strictEqual(response.body.length, 2)
+  assert.strictEqual(response.body.length, initialNotes.length)
 })
 
 test('a specific note is within the returned notes', async () => {
