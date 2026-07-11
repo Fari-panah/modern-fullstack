8ebb2a1c298c9a19978bfbf9a5fa05d9f3225ae5
Fari-panah <panahfari81@gmail.com>
2026-07-11 09:32:35 +0300

    add usersInDb to test_helper.js in notes

--- part4/notes/backend/app.js
+++ part4/notes/backend/app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
 const notesRouter = require('./controllers/notes')
+const usersRouter = require('./controllers/users')
 const middleware = require('./utils/middleware')
 
 const app = express()
--- part4/notes/backend/tests/note_api.test.js
+++ part4/notes/backend/tests/note_api.test.js
@@ -1,10 +1,12 @@
 const { test, after, beforeEach, describe } = require('node:test')
 const assert = require('node:assert')
+const bcrypt = require('bcrypt')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
 const helper = require('./test_helper')
 const Note = require('../models/note')
+const User = require('../models/user')
 
 const api = supertest(app)
 
@@ -107,6 +109,18 @@ test('a note can be deleted', async() => {
   assert(!ids.includes(noteToDelete.id))
   assert.strictEqual(notesAtEnd.length, helper.initialNotes.length -1)
 
+})
+describe('when there is initially one user at db', () => {
+  beforeEach(async () => {
+    await User.deleteMany({})
+    const passwordHash = await bcrypt.hash('sekret', 10)
+    const user = new User({ username: 'root', passwordHash })
+    await user.save()
+  })
+})
+test('creation succeeds with a fresh username', async () => {
+  const usersAtStart = await helper.usersInDb()
+  
 })
 
 
