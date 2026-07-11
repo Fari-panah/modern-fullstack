f122dab60fb46eb21b51e3d4b13d05fbc3b6ec77
Fari-panah <panahfari81@gmail.com>
2026-07-11 09:49:01 +0300

    test newUser is added in notes/tests/note_api.test.js

--- part4/notes/backend/tests/note_api.test.js
+++ part4/notes/backend/tests/note_api.test.js
@@ -120,7 +120,25 @@ describe('when there is initially one user at db', () => {
 })
 test('creation succeeds with a fresh username', async () => {
   const usersAtStart = await helper.usersInDb()
-  
+  const newUser = {
+    username: 'mluukkai',
+    name: 'Matti Luukkainen',
+    password: 'salainen',
+  }
+  await api
+    .post('/api/users')
+    .send(newUser)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const userSAtEnd = await helper.usersInDb()
+  assert.strictEqual(userSAtEnd.length, usersAtStart.length+1)
+
+  const usernames = userSAtEnd.map(u => u.username)
+  assert(usernames.includes(newUser.username))
+
+
+
 })
 
 
