e40a1d3616089d3d05d7a4deeb7c5621212b7b49
Fari-panah <panahfari81@gmail.com>
2026-07-17 13:17:56 +0300

    4.15: Blog List Expansion, step 3

--- part4/blogs/backend/tests/test_helper.js
+++ part4/blogs/backend/tests/test_helper.js
@@ -1,4 +1,5 @@
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 const initialBlogs = [
   {
@@ -19,6 +20,11 @@ const blogsInDb = async () => {
   return notes.map(note => note.toJSON())
 }
 
+const userInDb = async () => {
+  const users = await User.find({})
+  return users.map(u => u.toJSON())
+}
+
 module.exports = {
-  initialBlogs, blogsInDb
+  initialBlogs, blogsInDb, userInDb
 }
