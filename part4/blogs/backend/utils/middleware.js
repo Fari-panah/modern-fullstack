0be794ae81e58dd4acaf2bdc016c6997daa7433b
Fari-panah <panahfari81@gmail.com>
2026-08-09 20:04:28 +0300

    4.20*: Blog List Expansion, step 8

--- part4/blogs/backend/utils/middleware.js
+++ part4/blogs/backend/utils/middleware.js
@@ -12,6 +12,16 @@ const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
 
+const tokenExtractor = (request, response, next) => {
+  const authorization = request.get('authorization')
+
+  if (authorization && authorization.startsWith('Bearer ')) {
+    request.token = authorization.replace('Bearer ', '')
+  }
+  next()
+}
+
+
 const errorHandler = (error, request, response, next) => {
   logger.error(error.message)
 
@@ -23,11 +33,13 @@ const errorHandler = (error, request, response, next) => {
     return response.status(400).json({ error: 'expected `username` to be unique' })
   }else if (error.name ===  'JsonWebTokenError') {
     return response.status(401).json({ error: 'token invalid' })
+  }
   next(error)
 }
 
 module.exports = {
   requestLogger,
   unknownEndpoint,
+  tokenExtractor,
   errorHandler
 }
