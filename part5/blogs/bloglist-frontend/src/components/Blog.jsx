f3393f1b1730fdf551353a7c3d738f1d609b6154
Fari-panah <panahfari81@gmail.com>
2026-08-14 10:15:45 +0300

    Blog List Frontend, step 7

--- part5/blogs/bloglist-frontend/src/components/Blog.jsx
+++ part5/blogs/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,38 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+const Blog = ({ blog }) => {
+   const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+  const [view, setView] = useState(false)
+
+  const visibility = () => {
+    setView(!view)
+
+  }
+  return (
+    <div style={blogStyle}>      
+      <div>
+        {blog.title} 
+        <button onClick={visibility}>{view ? 'hide' : 'view'}</button>
+      </div>
+      {view && (
+        <div>
+          {blog.url}
+          <br />
+          likes {blog.likes}
+          <button>like</button>
+          <br />
+          {blog.author}
+        </div>
+      )}
+     
+    </div> 
+  )
+
+}
 
 export default Blog
