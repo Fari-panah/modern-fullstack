4bc9592f308151b13930b2f72a09134d9a1caec0
Fari-panah <panahfari81@gmail.com>
2026-08-19 10:02:49 +0300

    paging and navigation

--- part5/notes/frontend/src/App.jsx
+++ part5/notes/frontend/src/App.jsx
@@ -1,121 +1,57 @@
-import { useState, useEffect, useRef } from 'react'
-import Footer from './components/Footer'
-import Note from './components/Note'
-import Notification from './components/Notification'
-import loginService from './services/login'
+import { useState, useEffect } from 'react'
 import noteService from './services/notes'
+import {
+  BrowserRouter as Router,
+  Routes, Route, Link
+} from 'react-router-dom'
+import NoteList from './components/NoteList'
+import Home from './components/Home'
+import Footer from './components/Footer'
 import NoteForm from './components/NoteForm'
-import Togglable from './components/Togglable'
-import LoginForm from './components/LoginForm'
 
 const App = () => {
-  const [notes, setNotes] = useState([])
-  const [showAll, setShowAll] = useState(true)
-  const [errorMessage, setErrorMessage] = useState(null)
-  const [user, setUser] = useState(null)
-
-  const noteFormRef = useRef()
+   const [notes, setNotes] = useState([])
+  
 
   useEffect(() => {
     noteService.getAll().then(initialNotes => {
       setNotes(initialNotes)
     })
   }, [])
-  // checkthat a user was previously logged in
-  //empty array: ensures that the effect is executed only when 
-  // the component is rendered for the first time
-  useEffect(() => {
-    const loggedUserJSON = window.localStorage.getItem('loggedNoteappUser')
-    if (loggedUserJSON) {
-      const user = JSON.parse(loggedUserJSON)
-      setUser(user)
-      noteService.setToken(user.token)
-    }
-  }, [])
+ 
 
   const addNote = noteObject => {
-    noteFormRef.current.toggleVisibility()
+    //noteFormRef.current.toggleVisibility()
     noteService.create(noteObject).then(returnedNote => {
       setNotes(notes.concat(returnedNote))
     })
   }
-
-  const toggleImportanceOf = id => {
-    const note = notes.find(n => n.id === id)
-    const changedNote = { ...note, important: !note.important }
-
-    noteService
-      .update(id, changedNote)
-      .then(returnedNote => {
-        setNotes(notes.map(note => (note.id !== id ? note : returnedNote)))
-      })
-      .catch(() => {
-        setErrorMessage(
-          `Note '${note.content}' was already removed from server`
-        )
-        setTimeout(() => {
-          setErrorMessage(null)
-        }, 5000)
-        setNotes(notes.filter(n => n.id !== id))
-      })
+  const padding = {
+    padding: 5
   }
 
-  const handleLogin = async (username, password) => {
-   
-
-    try {
-      const user = await loginService.login({ username, password })
-       window.localStorage.setItem(
-        'loggedNoteappUser', JSON.stringify(user)
-      ) 
-      noteService.setToken(user.token)
-      setUser(user)
-    } catch {
-      setErrorMessage('wrong credentials')
-      setTimeout(() => {
-        setErrorMessage(null)
-      }, 5000)
-    }
-  }
-  const notesToShow = showAll ? notes : notes.filter(note => note.important)
-  
   return (
-    <div>
-      <h1>Notes</h1>
-      <Notification message={errorMessage} />
-
-      {!user && <Togglable buttonLabel="login">
-        <LoginForm
-          onLogin={handleLogin}
-        />
-      </Togglable>}
-      {user && (
-        <div>
-          <p>{user.name} logged in</p>
-          <Togglable buttonLabel="new note" ref={noteFormRef}>
-            <NoteForm createNote={addNote} />
-          </Togglable>
-        </div>
-      )}
-
+    <Router>
       <div>
-        <button onClick={() => setShowAll(!showAll)}>
-          show {showAll ? 'important' : 'all'}
-        </button>
+        <Link style={padding} to="/">home</Link>
+        <Link style={padding} to="/notes">notes</Link>
+        <Link style={padding} to="/create">new note</Link>
       </div>
-      <ul>
-        {notesToShow.map(note => (
-          <Note
-            key={note.id}
-            note={note}
-            toggleImportance={() => toggleImportanceOf(note.id)}
-          />
-        ))}
-      </ul>
+
+      <Routes>
+        <Route path="/notes" element={
+          <NoteList notes={notes} />
+        } />
+        <Route path="/create" element={
+          <NoteForm createNote={addNote}/>
+        } />
+        <Route path="/" element={<Home />} />
+      </Routes>
 
       <Footer />
-    </div>
+    </Router>
   )
+  
 }
 
 export default App
---
+++ part5/notes/frontend/src/components/NoteList.jsx
@@ -0,0 +1,105 @@
+import { useState, useEffect, useRef } from 'react'
+import Note from './components/Note'
+import Notification from './components/Notification'
+import loginService from './services/login'
+import noteService from './services/notes'
+import NoteForm from './components/NoteForm'
+import Togglable from './components/Togglable'
+import LoginForm from './components/LoginForm'
+
+const NoteList = ({ notes }) => {
+  const [showAll, setShowAll] = useState(true)
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [user, setUser] = useState(null)
+
+  const noteFormRef = useRef()
+
+   // checkthat a user was previously logged in
+  //empty array: ensures that the effect is executed only when 
+  // the component is rendered for the first time
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedNoteappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      noteService.setToken(user.token)
+    }
+  }, [])
+
+  const toggleImportanceOf = id => {
+    const note = notes.find(n => n.id === id)
+    const changedNote = { ...note, important: !note.important }
+
+    noteService
+      .update(id, changedNote)
+      .then(returnedNote => {
+       // setNotes(notes.map(note => (note.id !== id ? note : returnedNote)))
+      })
+      .catch(() => {
+        setErrorMessage(
+          `Note '${note.content}' was already removed from server`
+        )
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
+       // setNotes(notes.filter(n => n.id !== id))
+      })
+
+
+      const handleLogin = async (username, password) => {
+   
+
+    try {
+      const user = await loginService.login({ username, password })
+       window.localStorage.setItem(
+        'loggedNoteappUser', JSON.stringify(user)
+      ) 
+      noteService.setToken(user.token)
+      setUser(user)
+    } catch {
+      setErrorMessage('wrong credentials')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+  }
+  const notesToShow = showAll ? notes : notes.filter(note => note.important)
+  
+  return (
+    <div>
+      <h1>Notes</h1>
+      <Notification message={errorMessage} />
+
+      {!user && <Togglable buttonLabel="login">
+        <LoginForm
+          onLogin={handleLogin}
+        />
+      </Togglable>}
+      {user && (
+        <div>
+          <p>{user.name} logged in</p>
+          <Togglable buttonLabel="new note" ref={noteFormRef}>
+            <NoteForm createNote={addNote} />
+          </Togglable>
+        </div>
+      )}
+
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all'}
+        </button>
+      </div>
+      <ul>
+        {notesToShow.map(note => (
+          <Note
+            key={note.id}
+            note={note}
+            toggleImportance={() => toggleImportanceOf(note.id)}
+          />
+        ))}
+      </ul>
+    </div>
+  )
+  }
+}
+export default NoteList
